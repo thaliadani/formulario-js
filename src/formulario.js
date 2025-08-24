135d735520fb17040a38ab7f522c901281705871
Thalia Danielle <thaliadani2@gmail.com>
2025-08-24 12:12:21 -0300

    Atualiza formulário de login com melhorias na estrutura e estilo

--- src/formulario.js
+++ src/formulario.js
@@ -1,6 +1,11 @@
+const botaoEnviar = document.querySelector("#enviar");
+const nome = document.querySelector("#nome");
+const senha = document.querySelector("#senha");
 
-function enviarForm() {
-    event.preventDefault()
-
-    alert('Formulário enviado com sucesso!')
+function enviarForm(event){
+    event.preventDefault();
+    console.log("Nome: " + nome.value);
+    console.log("Senha: " + senha.value);
 }
+
+botaoEnviar.addEventListener("click", enviarForm);
